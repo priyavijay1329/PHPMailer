460293cc28345cecb2f15660373f750cf83e28ac
Priya baskar <vishnupriya.baskar@salcomp.com>
2020-12-02 15:06:28 +0530

    php mailer 02.12.2020

---
+++ PHPMailer/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<br></br><br></br><p align="left" >
+		 <font color="darkblue" > Access Denied!!!</font><br><br></p>
+		<p align="left"  >The URL you have provided could not be reached.</font></p>
+		  <p align="left" > Please verify that the URL is correct.
+		</font><br><br></p>
+	</body>
+</html>
